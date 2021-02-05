8ab414b70da1fbd035aa44754838979c111c44ca
Dan Rowe <hello@danrowe.me>
2021-02-05 08:27:16 +0000

    Feature: Herd Recordings - convert relevant data

---
+++ __Tests__/deciferDatastream/herdRecordings.test.ts
@@ -0,0 +1,66 @@
+/* eslint-disable no-undef */
+import fs from 'fs/promises';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'jest-extended';
+import herdRecordings from '../../src/deciferDatastream/herdRecordings';
+
+let datastream = '';
+
+beforeAll(async () => {
+  datastream = await fs.readFile('./__Tests__/information/DSMEMBER.DAT', 'utf8');
+
+  return datastream;
+});
+
+test('Returns an array containing herd recordings', () => {
+  expect(herdRecordings(datastream)).toBeArray();
+});
+
+test('Each recording should have expected types within the object', () => {
+  const data = herdRecordings(datastream);
+  expect(data).toEqual(
+    expect.arrayContaining([
+      expect.toContainKeys([
+        'date',
+        'totalAnimals',
+        'cowsInMilk',
+        'cowsIn3xMilk',
+        'totalWeightOfMilk',
+        'totalWeightOfFat',
+        'totalWeightOfProtein',
+        'totalWeightOfLactose',
+        'differenceReason',
+        'numberMissedWeights',
+        'printEligible',
+        'bulkYield',
+        'bulkFatPercentage',
+        'bulkProteinPercentage',
+        'bulkLactosePercentage',
+        'herdProductionBase',
+        'bulkSCC',
+      ]),
+    ]),
+  );
+});
+
+test('First recording should have expected values', () => {
+  const data = herdRecordings(datastream);
+  expect(data[0]).toEqual(
+    expect.toContainEntries([['bulkYield', 3311], ['totalAnimals', 183], ['cowsInMilk', 144], ['date', new Date('2006-11-23T00:00:00.000Z')]]),
+  );
+});
+
+test('Throws exception to a random data string', () => {
+  function randomString() {
+    herdRecordings('abcdefghijk');
+  }
+
+  expect(randomString).toThrow();
+});
+
+test('Throws exception to invalid herd information string', () => {
+  function invalidString() {
+    herdRecordings('HD,a,b,c,HD,C1');
+  }
+  expect(invalidString).toThrow();
+});
