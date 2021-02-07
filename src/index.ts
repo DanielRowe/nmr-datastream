66ffb69541b69001bd0ffa7224b45a693578bfbf
Dan Rowe <hello@danrowe.me>
2021-02-07 19:10:55 +0000

    Added reference to functions for global usage

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import { openDatastream } from './openDatastream';
+import { bullList } from './deciferDatastream/bullList';
+import { cowList } from './deciferDatastream/cowInformation';
+import { deadDams as deadDamList } from './deciferDatastream/deadDams';
+import { herdInformation } from './deciferDatastream/herdInformation';
+import { herdRecordings } from './deciferDatastream/herdRecordings';
+import { lactationsList } from './deciferDatastream/lactationInformation';
+import { statementInformation } from './deciferDatastream/statementInformation';
+
 import utils from './utils/index';
 
 export {
   openDatastream,
+  bullList,
+  cowList,
+  deadDamList,
+  herdInformation,
+  herdRecordings,
+  lactationsList,
+  statementInformation,
   utils,
 };
