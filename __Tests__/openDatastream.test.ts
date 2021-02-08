dd28c5ad090dcab368d924984bff361fbe7f214b
Dan Rowe <hello@danrowe.me>
2021-02-08 14:47:39 +0000

    Improved test coverage

--- __Tests__/openDatastream.test.ts
+++ __Tests__/openDatastream.test.ts
@@ -4,15 +4,14 @@ import { openDatastream } from '../src/openDatastream';
 
 test('Opens a .DAT datastream file from file path', async () => {
   const data = await openDatastream('./__Tests__/information/DSMEMBER.DAT');
-
-  expect(data).toHaveLength(769470); // Example file has that many characters.
+  expect(data).toHaveLength(769626); // Example file has that many characters.
 });
 
 test('Opens a DAT datastream from buffer', async () => {
   const file = await fs.readFile('./__Tests__/information/DSMEMBER.DAT');
   const data = await openDatastream(file);
 
-  expect(data).toHaveLength(769470); // Example file has that many characters.
+  expect(data).toHaveLength(769626); // Example file has that many characters.
 });
 
 test('Open a .EXE from file path', async () => {
@@ -29,7 +28,7 @@ test('Opens a EXE from buffer', async () => {
 });
 
 test('Expect error when invalid EXE passed', async () => {
-  await expect(openDatastream('./__Tests__/information/fake.exe')).rejects.toThrowError();
+  await expect(openDatastream('./__Tests__/information/fake.exe')).rejects.toThrowError('Invalid file provided');
 });
 
 test('Expect error when no DSMEMBER.DAT is found within self extracting exe', async () => {
