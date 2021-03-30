ce06c417ee7e4e3adc4e990485831ff40a28d0e1
Dan Rowe <hello@danrowe.me>
2021-03-30 21:00:03 +0100

    Fix: When SW bred code used in statement info

--- src/deciferDatastream/statementInformation.ts
+++ src/deciferDatastream/statementInformation.ts
@@ -94,8 +94,10 @@ export const statementInformation = (datastream: string, afterDate: Date = new D
 
   statementInfoFromDatastream.map((info) => {
     const prependOne = `1,${info}`; // Adds a 1 to the first line - means we can latter identify type in switch.
-    const extraNumbering = prependOne.replace(/S([A-Z0-9]{1})/g, 'S$1$1'); // during split we loose the row number - add an extra for good measure.
-    const rows = extraNumbering.split(/S[A-Z0-9]{1}/g); // Split down the rows.
+    // On the next to lines the check for not having 12 digits
+    // after is if SW is used as a breed code on service info
+    const extraNumbering = prependOne.replace(/S([A-Z0-9]{1}),(?![A-Z0-9\s]{12})/g, 'S$1,$1,'); // during split we loose the row number - add an extra for good measure.
+    const rows = extraNumbering.split(/S[A-Z0-9]{1},(?![A-Z0-9\s]{12})/g); // Split down the rows.
     let cow: CowInfo = {
       milkSamples: [],
       services: [],
